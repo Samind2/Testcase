0b0cd9f60a53d37cc2acabda46f84e2a07128c71
Samind2 <111554627+Samind2@users.noreply.github.com>
2024-02-22 15:43:00 +0700

    Add files via upload

--- testcustonform.py
+++ testcustonform.py
@@ -30,6 +30,7 @@ class TestCustomerForm(unittest.TestCase):
         
         result = self.driver.find_element(By.ID, "firstName").text
         self.assertEqual("First Name: johnjohn", result)
+        self.driver.save_screenshot('img/testjhone1.png')
         
     def test_input2(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
@@ -51,6 +52,8 @@ class TestCustomerForm(unittest.TestCase):
         
         result = self.driver.find_element(By.ID, "firstName").text
         self.assertEqual("First Name: johnj", result)
+        self.driver.save_screenshot('img/testjhone2.png')
+        
 
     def test_input3(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
@@ -72,6 +75,30 @@ class TestCustomerForm(unittest.TestCase):
         
         result = self.driver.find_element(By.ID, "firstName").text
         self.assertEqual("First Name: johnjo", result)
+        self.driver.save_screenshot('img/testjhone3.png')
+
+        
+    def test_input4(self):
+        self.driver.get("http://127.0.0.1/customerphp/customer.php")
+        
+        name = self.driver.find_element(By.ID, "firstName")
+        last = self.driver.find_element(By.ID, "lastName")
+        age = self.driver.find_element(By.ID, "age")
+        drp_title = Select(self.driver.find_element(By.ID, "title"))
+        drp_title.select_by_index(0)
+        
+        name.send_keys("johnjohnjohnjo")
+        last.send_keys("canoncan")
+        age.send_keys("150")
+
+        submit = self.driver.find_element(By.ID, "submit")
+        submit.click()
+
+        time.sleep(1) # Give some time for the page to update
+        
+        result = self.driver.find_element(By.ID, "firstName").text
+        self.assertEqual("First Name: johnjohnjohnjo", result)
+        self.driver.save_screenshot('img/testjhone4.png')
        
         
     def test_input5(self):
@@ -94,6 +121,7 @@ class TestCustomerForm(unittest.TestCase):
         
         result = self.driver.find_element(By.ID, "firstName").text
         self.assertEqual("First Name: johnjohnjohnjoh", result)
+        self.driver.save_screenshot('img/testjhone5.png')
 
     def test_input6(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
@@ -114,31 +142,10 @@ class TestCustomerForm(unittest.TestCase):
 
         time.sleep(1) # Give some time for the page to update
     
-        result = self.driver.find_element(By.ID, "firstName").text
-        self.assertEqual("First Name: john", result)  # Adjust assertion for invalid data
+        result = self.driver.find_element(By.ID, "formname").text
+        self.assertEqual("Customer Detail Form", result)  # Adjust assertion for invalid data
+        self.driver.save_screenshot('img/testjhone6.png')
 
-#    def test_input7(self):
-#        self.driver.get("http://127.0.0.1/customerphp/customer.php")
-        
-#        name = self.driver.find_element(By.ID, "firstName")
-#        last = self.driver.find_element(By.ID, "lastName")
-#        age = self.driver.find_element(By.ID, "age")
-#        drp_title = Select(self.driver.find_element(By.ID, "title"))
-#        drp_title.select_by_index(0)
-     
-    # Invalid data
-#        name.send_keys("johnjohnjohnjohn")  # Invalid first name
-#        last.send_keys("canoncan")
-#        age.send_keys("75")
-
-#        submit = self.driver.find_element(By.ID, "submit")
-#        submit.click()
-
-#        time.sleep(1) # Give some time for the page to update
-    
-#        result = self.driver.find_element(By.ID, "firstName").text
-#        self.assertEqual("First Name: johnjohnjohnjohn", result)  # Adjust assertion for invalid data
-        
     def test_input8(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
         
@@ -158,30 +165,9 @@ class TestCustomerForm(unittest.TestCase):
 
         time.sleep(1) # Give some time for the page to update
     
-        result = self.driver.find_element(By.ID, "firstName").text
-        self.assertEqual("First Name: johnjohn", result)  # Adjust assertion for invalid data
-        
-#    def test_input9(self):
-#        self.driver.get("http://127.0.0.1/customerphp/customer.php")
-        
-#        name = self.driver.find_element(By.ID, "firstName")
-#        last = self.driver.find_element(By.ID, "lastName")
-#        age = self.driver.find_element(By.ID, "age")
-#        drp_title = Select(self.driver.find_element(By.ID, "title"))
-#        drp_title.select_by_index(0)
-    
-    # Invalid data
-#        name.send_keys("johnjohn")  # Invalid first name
-#        last.send_keys("canoncanoncanon")
-#        age.send_keys("75")
-
-#        submit = self.driver.find_element(By.ID, "submit")
-#        submit.click()
-
-#        time.sleep(1) # Give some time for the page to update
-    
-#        result = self.driver.find_element(By.ID, "firstName").text
-#        self.assertEqual("First Name: johnjohn", result)  # Adjust assertion for invalid data
+        result = self.driver.find_element(By.ID, "formname").text
+        self.assertEqual("Customer Detail Form", result)  # Adjust assertion for invalid data
+        self.driver.save_screenshot('img/testjhone8.png')
         
     def test_input10(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
@@ -202,8 +188,9 @@ class TestCustomerForm(unittest.TestCase):
 
         time.sleep(1) # Give some time for the page to update
     
-        result = self.driver.find_element(By.ID, "firstName").text
-        self.assertEqual("First Name: johnjohn", result)  # Adjust assertion for invalid data
+        result = self.driver.find_element(By.ID, "formname").text
+        self.assertEqual("Customer Detail Form", result)  # Adjust assertion for invalid data
+        self.driver.save_screenshot('img/testjhone10.png')
 
     def test_input11(self):
         self.driver.get("http://127.0.0.1/customerphp/customer.php")
@@ -226,11 +213,12 @@ class TestCustomerForm(unittest.TestCase):
     
         result = self.driver.find_element(By.ID, "firstName").text
         self.assertNotEqual("First Name: johnjohn", result)  # Adjust assertion for invalid data
+        self.driver.save_screenshot('img/testjhone11.png')
 
     def tearDown(self):
-        self.driver.save_screenshot('testjhone11.png')
         self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
+
 
